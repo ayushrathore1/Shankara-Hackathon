be8fbcbeb382a061514e7b774f5ce42b11265d56
ayush rathore <rathoreayush512@gmail.com>
2026-02-28 12:38:27 +0530

    update

--- codelearnn-frontend/src/services/EventTracker.js
+++ codelearnn-frontend/src/services/EventTracker.js
@@ -9,7 +9,7 @@
 
 const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:5000/api'
-  : 'https://api2.codelearnn.com/api';
+  : 'https://api3.codelearnn.com/api';
 
 class EventTrackerService {
   constructor() {
